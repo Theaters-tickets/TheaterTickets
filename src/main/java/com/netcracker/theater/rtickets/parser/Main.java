928c3859610fd04cad393ce3eac460b244ab7489
kate7sv <kate7sv@mail.ru>
2021-10-31 23:18:25 +0300

    созданы классы для таблиц бд созданы DAO для классов добавлены зависимости в pom добавлен шаблон для разработки web изменена структура проекта добавлен lombok

--- src/main/java/com/netcracker/theater/rtickets/parser/Main.java
+++ src/main/java/com/netcracker/theater/rtickets/parser/Main.java
@@ -7,10 +7,10 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Play> arr = TESTPARSER.parsePlay();
-        for (Play plays : arr) {
-            System.out.println(plays);
-        }
+//        ArrayList<Play> arr = TESTPARSER.parsePlay();
+//        for (Play plays : arr) {
+//            System.out.println(plays);
+//        }
         /*
         try {
             FileWriter writer = new FileWriter("C:\\Users\\79501\\Documents\\PROJECT\\Project\\src\\main\\java\\com\\netcracker\\theater\\rtickets\\parser\\tags.txt", true);
