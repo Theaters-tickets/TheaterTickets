9e72f87a7899be68c1cd031c8a4b15d23cac0f62
Alisa <alica.2001@mail.ru>
2021-11-07 03:36:43 +0300

    Parser Theaters, Performances, Actors, Pictures, Categories ERRORS to fix: duplication in Performances, not unique values in categories

--- src/main/java/com/netcracker/theater/rtickets/parser/Main.java
+++ src/main/java/com/netcracker/theater/rtickets/parser/Main.java
@@ -1,17 +1,8 @@
 package com.netcracker.theater.rtickets.parser;
 
 import com.netcracker.theater.rtickets.NetcrackerTheaterTicketsApplication;
-import com.netcracker.theater.rtickets.data.entity.Repertoire;
-import com.netcracker.theater.rtickets.data.entity.Theatre;
-import com.netcracker.theater.rtickets.data.service.PerfomanceService;
-import com.netcracker.theater.rtickets.data.service.RepertoireService;
-import com.netcracker.theater.rtickets.data.service.TheatreService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
-
-
-import java.time.Instant;
-import java.util.ArrayList;
 
 
 public class Main {
@@ -21,46 +12,9 @@ public class Main {
         //System.out.println(Instant.now().getEpochSecond());
         ApplicationContext context= SpringApplication.run(NetcrackerTheaterTicketsApplication.class);
         ParserClass parserClass = context.getBean(ParserClass.class);
-        TESTPARSER testparser = context.getBean(TESTPARSER.class);
+        //TESTPARSER testparser = context.getBean(TESTPARSER.class);
         parserClass.parseTheatre();
         parserClass.parseRepertoire();
 
-
-
-/*
-        ArrayList<Repertoire> repertoires = TESTPARSER.parseRepertoire();
-        for (Repertoire r : repertoires) {
-            repertoireService.saveRep(r);
-        }
-        repertoires.clear();
-
-        ArrayList<Theatre> theatres = ParserClass.parseTheatre();
-        for (Theatre t : theatres) {
-            theatreService.saveTheatre(t);
-        }
-        System.out.println("END!");
-        */
-
-
-
-
-        /*
-        try {
-            FileWriter writer = new FileWriter("C:\\Users\\79501\\Documents\\PROJECT\\Project\\src\\main\\java\\com\\netcracker\\theater\\rtickets\\parser\\tags.txt", true);
-
-            HashSet<String> tags = new HashSet<String>();
-            ArrayList<Play> arr = ParserClass.parsePlay();
-            for (Play plays : arr) {
-                for (String tag : plays.getTags()) {
-                    tags.add(tag);
-                }
-            }
-            for (String tag :  tags) {
-                writer.append(tag + "\n");
-            }
-            writer.flush();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }*/
     }
 }
