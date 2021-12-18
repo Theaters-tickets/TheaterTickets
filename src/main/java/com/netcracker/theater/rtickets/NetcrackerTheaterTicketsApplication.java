d094a6fda19eb0c912f6f4478915546d5b4372c1
kate7sv <kate7sv@mail.ru>
2021-12-18 11:28:50 +0300

    добавлен log4j 2.16.0

--- src/main/java/com/netcracker/theater/rtickets/NetcrackerTheaterTicketsApplication.java
+++ src/main/java/com/netcracker/theater/rtickets/NetcrackerTheaterTicketsApplication.java
@@ -3,15 +3,6 @@ package com.netcracker.theater.rtickets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaDelete;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.CriteriaUpdate;
-import javax.persistence.metamodel.Metamodel;
-import java.util.List;
-import java.util.Map;
-
 @SpringBootApplication
 public class NetcrackerTheaterTicketsApplication {
 
