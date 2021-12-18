ce9368bf274a10a1398f48fd6fabd8fdcb8029d7
Alisa <alica.2001@mail.ru>
2021-12-19 00:58:04 +0300

    Странички под ошибки и контроллер (предыдущий закомментировала, возможно их можно совместить)

---
+++ src/main/java/com/netcracker/theater/rtickets/exception/NewErrorController.java
@@ -0,0 +1,32 @@
+package com.netcracker.theater.rtickets.exception;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class NewErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(Map<String, Object> model, Exception ex, HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String errorMessage = HttpStatus.INTERNAL_SERVER_ERROR.value() + " " + (ex.getMessage() != null ? ex.getMessage() : "Unknown page");
+        model.put("errorMessage","Error code: "+ errorMessage);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500";
+            }
+        }
+        return "error";
+    }
+}
